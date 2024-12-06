882180d13a6e9019b19a3583cc9ce057b716eae1
Anja Reusch <a.reusch.94@googlemail.com>
2024-12-06 17:03:16 +0200

    added a filter for go to verbs

--- scripts.js
+++ scripts.js
@@ -14,7 +14,6 @@ function readTextFile(file, callback) {
 }
 
 function filter_verb_list(allowed_tenses, selected_binyan, verb_list) {
-    console.log(verb_list)
     if(old_tenses != allowed_tenses || old_binyan != selected_binyan){
         verb_list_filtered = verb_list.filter((elem)=>{
             return allowed_tenses.indexOf(elem['tense']) >= 0 && selected_binyan == (elem['binyan'])
@@ -33,14 +32,25 @@ function get_new_verb(tenses_list, binyanim_list, verb_list){
     binyan_index = Math.floor(Math.random() * binyanim_list_length)
 
     selected_binyan = binyanim_list[binyan_index]
-    verb_list_filtered = filter_verb_list(allowed_tenses, selected_binyan, verb_list)
+
+    verb_list_filtered = filter_verb_list(tenses_list, selected_binyan, verb_list)
 
     verb_list_length = verb_list_filtered.length
     verb_index = Math.floor(Math.random() * verb_list_length)
 
-    console.log(verb_list_filtered[verb_index])
-
     return verb_list_filtered[verb_index]
 
+
+}
+
+function filter_go_to_verbs(verb_list){
+    go_to_verbs = ['להרגיש', 'לדבר', 'להתלבש', 'לגור', 'לקנות', 'להיגמר', 'לסגור', 'לשבת']
+    
+    
+    verb_list_filtered = verb_list.filter((elem)=>{
+        return go_to_verbs.indexOf(elem['infinitive_no_niqqud']) >= 0
+    })
+
+    return verb_list_filtered
 
 }
